5eba329054f9e5e6f3a2f859ecc90f13e7991220
arjun_raf <arjunhornet@gmail.com>
2023-11-13 21:06:26 +0100

    Code cleanup done.

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,58 @@
 import random
 
-
-def function_A(min, max):
-    """
-    Random integer.
-    """
+def generate_random_number(min, max):
+    """Returns a random integer between two specified integers"""
     return random.randint(min, max)
 
-
-def function_B():
+def generate_operation():
+    """Randomly returns an arithmetical operator sign from a fixed list"""
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def solver(first_number, second_number, arith_operation):
+    """
+    Function to return the calculated result of the arithmetic operation
+    chosen by the user.
+    """
+    problem_string = f"{first_number} {arith_operation} {second_number}"
+    if arith_operation == '+': 
+        answer = first_number + second_number
+    elif arith_operation == '-': 
+        answer = first_number - second_number
+    else: 
+        answer = first_number * second_number
+    return problem_string, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    user_points = 0
+    total_questions = 10
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problems, and you need to provide "\
+          "the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for question_num in range(1,total_questions+1):
+        
+        first_number = generate_random_number(1, 10)
+        second_number = generate_random_number(1, 10)
+        arith_operation = generate_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        PROBLEM, ANSWER = solver(first_number, second_number, arith_operation)
+        print(f"\nQuestion {question_num}: {PROBLEM}")
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
-
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+        ###Try and except block to catch invalid user input###
+        try:
+            useranswer = int(useranswer)
+            if useranswer == ANSWER: 
+                print("Correct! You earned a point.")
+                user_points = user_points + 1
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.")
+        except Exception as e:
+            print(f"""
+                  You entered a non-number string! Try again. 
+                  Error Message: {e}
+                  """)
+    print(f"\nGame over! Your score is: {user_points}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
