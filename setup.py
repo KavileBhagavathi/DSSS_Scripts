800034a81e8f42293e90c24253fec1b65909eb68
arjun_raf <arjunhornet@gmail.com>
2023-11-13 22:34:57 +0100

    added requirements file and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='math_quiz',
+   version='1.0.0',
+   description='A hobby math quiz module',
+   author='Arjun_Lenan_Sandhya',
+   author_email='arjun.lenan.sandhya@fau.de',
+   packages=['math_quiz'],
+   install_requires=['random', 'unittest']
+)
